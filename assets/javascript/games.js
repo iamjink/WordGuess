4a6f610cf8c1ff86fae480755eba35bb8b66c8c2
iamjink <jinijinjin@gmail.com>
2020-01-14 17:32:43 -0800

    function code for game reset and game rules

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,28 +1,18 @@
 //jquery document.ready 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
 var songChoices = ['baby one more time', 'oops! i did it again', 'crazy', 'stronger', 'work bitch!', 'from the bottom of my broken heart', 'everytime', 'make me','piece of me', 'if you seek amy', 'womanizer', 'circus', 'i wanna go', '3', 'hold it against me', 'scream & shout'];
 
-var win = 0;
+var wins = 0;
 var guessCountdown = 10;
 var letterPressed = [];
+var WordGuess = [];
 var computerWord = songChoices[Math.floor(Math.random() * songChoices.length)].toUpperCase();
 var GameEnd = false;
 
-//replaces guessCount and Letters typed text
-function userGuessCount() {
-    document.querySelector("#guessCountdown").innerHTML = guessCountdown;
-}
-function userText() {
-    document.querySelector("#userGuess").innerHTML = letterPressed;
-}
-
-//defining reset function
-var reset = function() {
-    guessCountdown = 10;
-    letterPressed = [];
-}
+//reset function starts on web, this function is defined at the very bottom
+reset();
 
 //if key pressed is A-Z range, as stated in function isAlpha, then run letterMatch function upon keypress
 document.onkeypress = function(event){
@@ -32,43 +22,67 @@ document.onkeypress = function(event){
 }
 
 //game function. check to see if letter is in compputerWord
-function letterMatch(){
+function letterMatch(letter){
+    letterMatch = false; 
+    for (i=0, j=computerWord.length; i<j; i++) {
+        if (letter === computerWord[i]) {
+            WordGuess[i] = letter;
+            letterMatch = true;
+
+            if (WordGuess.join("")===computerWord) {
+                wins++;
+                GameEnd = true;
+            };
+        };
+    };
+
+    if (!letterMatch) {
+        if (!letterPressed.includes(letter)) {
+            letterPressed.push(letter)
+            guessCountdown--;
+        }
+
+        if (guessCountdown === 0) {
+            letterPressed = computerWord.split()
+            GameEnd = true;
+        }
+    }
+
+    updateText()
 
 }
 
+//reset game function
+
+function reset() {
+    GameEnd = false;
+
+    //resetting words and letters
+    letterPressed = [];
+    WordGuess = [];
+
+    for(i=0, j=computerWord.length; i<j; i++) {
+        if (computerWord[i]==="") {
+            letterPressed.push("")
+        } else {
+            letterPressed.push("_")
+        }
+    }
+    updateText();
+}
 
 //check to see if key pressed by user is A-Z
 function isAlpha(ch){
     return/^[A-Z]$/i.test(ch);
 }
 
-// document.onkeyup = function(event) {
-//     guessCountdown--;
+//function for updating text in browser in each category
+function updateText() {
+    document.querySelector("winsNum").innerText = wins;
+    document.querySelector("currentWord").innerText = WordGuess.join("");
+    document.querySelector("guessCountdown").innerText = guessCountdown;
+    document.querySelector("userGuess").innerText = letterPressed.join("");
 
+}
 
-//     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    
-    
-//     letterPressed.push(userGuess);
-
-
-//     userGuessCount();
-//     userText(); 
-
-   
-// if (userGuess === computerChoices) {
-//     win++;
-//     document.querySelector("#wins").innerHTML = "Wins: " + win;
-
-   
-//     reset();
-// }
-
-// else if (guessCountdown === 0){
-//     reset();
-// }
-
-// };
-
-
-});
+// });
